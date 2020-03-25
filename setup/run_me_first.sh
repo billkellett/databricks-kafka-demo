b0f974970588bcd99f6f09257914b5d4d6146de9
billkellett <44028163+billkellett@users.noreply.github.com>
2020-03-25 14:03:33 -0400

    Add files via upload

---
+++ setup/run_me_first.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+ 
+echo "Downloading scripts..."
+curl -o install_kafka.sh 'https://raw.githubusercontent.com/billkellett/databricks-kafka-demo/master/setup/install_kafka.sh' -L 
+curl -o start_kafka.sh 'https://raw.githubusercontent.com/billkellett/databricks-kafka-demo/master/setup/start_kafka.sh' -L 
+curl -o stop_kafka.sh 'https://raw.githubusercontent.com/billkellett/databricks-kafka-demo/master/setup/stop_kafka.sh' -L 
+curl -o verify_kafka_running.sh 'https://raw.githubusercontent.com/billkellett/databricks-kafka-demo/master/setup/verify_kafka_running.sh' -L 
+curl -o create_kafka_topics.sh 'https://raw.githubusercontent.com/billkellett/databricks-kafka-demo/master/setup/create_kafka_topics.sh' -L 
+
+chmod +x install_kafka.sh
+chmod +x start_kafka.sh
+chmod +x stop_kafka.sh
+chmod +x verify_kafka_running.sh 
+chmod +x create_kafka_topics.sh 
+
+echo "... scripts downloaded."
+
+echo "Downloading data for event generator..."
+mkdir data
+curl -o data/spark_streaming_workshop.rating_modifiers.csv 'https://github.com/billkellett/databricks-kafka-demo/blob/master/setup/data/spark_streaming_workshop.rating_modifiers.csv' -L 
+echo "... data download complete."
+echo "Finished."
+
