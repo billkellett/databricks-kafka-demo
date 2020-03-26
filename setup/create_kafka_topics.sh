2bde31276b42c4f5da9eb81bfb355f5d85c51d04
billkellett <44028163+billkellett@users.noreply.github.com>
2020-03-26 09:28:19 -0400

    Update create_kafka_topics.sh

--- setup/create_kafka_topics.sh
+++ setup/create_kafka_topics.sh
@@ -3,7 +3,7 @@
 # Create kafka topics
 echo "Creating Kafka topics..."
 kafka/kafka_2.12-2.4.1/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic rating_modifiers
-kafka/kafka_2.12-2.5.1/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic good_news
-kafka/kafka_2.12-2.5.1/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic bad_news
-kafka/kafka_2.12-2.5.1/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic test
+kafka/kafka_2.12-2.4.1/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic good_news
+kafka/kafka_2.12-2.4.1/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic bad_news
+kafka/kafka_2.12-2.4.1/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic test
 echo "... Kafka topics created."
