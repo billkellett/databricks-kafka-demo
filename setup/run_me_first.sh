3c349d2c95232bed120fe8610f6c4b288e39d05a
billkellett <44028163+billkellett@users.noreply.github.com>
2020-04-02 18:24:10 -0400

    Add files via upload

--- setup/run_me_first.sh
+++ setup/run_me_first.sh
@@ -6,7 +6,7 @@ curl -o start_kafka.sh 'https://raw.githubusercontent.com/billkellett/databricks
 curl -o stop_kafka.sh 'https://raw.githubusercontent.com/billkellett/databricks-kafka-demo/master/setup/stop_kafka.sh' -L 
 curl -o verify_kafka_running.sh 'https://raw.githubusercontent.com/billkellett/databricks-kafka-demo/master/setup/verify_kafka_running.sh' -L 
 curl -o create_kafka_topics.sh 'https://raw.githubusercontent.com/billkellett/databricks-kafka-demo/master/setup/create_kafka_topics.sh' -L 
-curl -o README_Kafka-server-setup.txt 'https://raw.githubusercontent.com/billkellett/databricks-kafka-demo/master/README-Kafka-server-setup.txt' -L 
+curl -o README_Kafka-demo.txt 'https://raw.githubusercontent.com/billkellett/databricks-kafka-demo/master/README-Kafka-demo.txt' -L 
 
 chmod +x install_kafka.sh
 chmod +x start_kafka.sh
@@ -15,6 +15,10 @@ chmod +x verify_kafka_running.sh
 chmod +x create_kafka_topics.sh 
 
 echo "... scripts downloaded."
+
+echo "Downloading event generator application..."
+curl -o kafka-event-generator-1.0-SNAPSHOT-jar-with-dependencies.jar 'https://raw.githubusercontent.com/billkellett/databricks-kafka-demo/master/setup/kafka-event-generator-1.0-SNAPSHOT-jar-with-dependencies.jar' -L 
+echo "... event generator application download complete."
 
 echo "Downloading data for event generator..."
 mkdir data
